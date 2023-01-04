c7281eb1130fae6dee8bb60494f36c88199aac99
Veeti K <veeti@veetik.com>
2023-01-04 17:50:10 +0200

    export stuff related to modelExercise fields from db package

---
+++ packages/db/src/modelExerciseFields.ts
@@ -0,0 +1,16 @@
+export const modelExerciseFields = {
+	weight: BigInt(1),
+	reps: BigInt(2),
+	time: BigInt(4),
+	distance: BigInt(8),
+	kcal: BigInt(16),
+};
+
+export const hasExerciseField = (
+	exerciseFields: bigint,
+	field: keyof typeof modelExerciseFields
+) => {
+	const fieldAsBigInt = modelExerciseFields[field];
+
+	return (exerciseFields & fieldAsBigInt) === fieldAsBigInt;
+};
