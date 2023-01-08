52039b4c2bd343ee355d5637fcabed0514a66c3d
Veeti K <veeti@veetik.com>
2023-01-08 04:51:04 +0200

    back to css transitions

--- apps/frontend/src/components/_ui/Modal.tsx
+++ apps/frontend/src/components/_ui/Modal.tsx
@@ -1,8 +1,5 @@
-import { Dialog } from "@headlessui/react";
-import { AnimatePresence, motion } from "framer-motion";
-import { ReactNode, useRef, useState } from "react";
-
-import { Card } from "./Cards/Card";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, ReactNode, useRef, useState } from "react";
 
 type Props = {
 	title: string;
@@ -15,55 +12,52 @@ export const Modal = ({ title, children, closeModal, isOpen }: Props) => {
 	const initialFocusRef = useRef(null);
 
 	return (
-		<AnimatePresence>
-			{isOpen && (
-				<Dialog
-					static
-					open={isOpen}
-					className="z-10"
-					onClose={() => closeModal()}
-					initialFocus={initialFocusRef}
+		<Transition appear show={isOpen} as={Fragment}>
+			<Dialog
+				as="div"
+				className="relative z-10"
+				onClose={closeModal}
+				initialFocus={initialFocusRef}
+			>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-[90ms]"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-[110ms]"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
 				>
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{
-							opacity: 1,
-							transition: { duration: 0.09 },
-						}}
-						exit={{ opacity: 0, transition: { duration: 0.1 } }}
-						className="bg-primary-1200/50 fixed inset-0 backdrop-blur-sm"
-					/>
+					<div className="fixed inset-0 bg-black/40" />
+				</Transition.Child>
 
-					<Dialog.Panel
-						key={title}
-						className="absolute left-1/2 top-1/2 w-full max-w-sm -translate-x-1/2 -translate-y-[100%] rounded-xl p-2 shadow-xl"
-					>
-						<Card
-							as={motion.div}
-							initial={{ opacity: 0, scale: 0.97 }}
-							animate={{
-								opacity: 1,
-								y: 0,
-								scale: 1,
-								height: "auto",
-								transition: { duration: 0.1 },
-							}}
-							exit={{ opacity: 0, scale: 0.97, transition: { duration: 0.1 } }}
+				<div className="fixed inset-0 overflow-y-auto">
+					<div className="flex min-h-full items-center justify-center p-3">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-[100ms]"
+							enterFrom="opacity-0 scale-[96%]"
+							enterTo="opacity-100 scale-100"
+							leave="ease-in duration-[110ms]"
+							leaveFrom="opacity-100 scale-100"
+							leaveTo="opacity-0 scale-[96%]"
 						>
-							<Dialog.Title
-								ref={initialFocusRef}
-								as="h3"
-								className="px-4 pt-4 text-lg font-medium leading-6"
-							>
-								{title}
-							</Dialog.Title>
+							<Dialog.Panel className="border-primary-800 bg-primary-1100 w-full max-w-sm transform overflow-hidden rounded-xl border shadow-xl transition-all">
+								<Dialog.Title
+									as="h1"
+									className="px-4 pt-4 text-lg font-medium leading-6"
+									ref={initialFocusRef}
+								>
+									{title}
+								</Dialog.Title>
 
-							{children}
-						</Card>
-					</Dialog.Panel>
-				</Dialog>
-			)}
-		</AnimatePresence>
+								{children}
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition>
 	);
 };
 
