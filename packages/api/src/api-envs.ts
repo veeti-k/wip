6de6c50718c0b861f5419792e91810cc781f9e6a
Veeti K <veeti@veetik.com>
2023-01-08 06:42:42 +0200

    remove envs

--- packages/api/src/api-envs.ts
+++ packages/api/src/api-envs.ts
@@ -1,12 +1,6 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-	SMTP_HOST: z.string(),
-	SMTP_PORT: z.string().transform(Number),
-	SMTP_USER: z.string(),
-	SMTP_PASS: z.string(),
-	SMTP_FROM: z.string(),
-
 	JWT_SECRET: z.string().transform((s) => new TextEncoder().encode(s)),
 	JWT_ISSUER: z.string(),
 	JWT_AUDIENCE: z.string(),
