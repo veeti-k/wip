628f9d3f697045baf009250b7dabd24eb5262e38
Veeti K <veeti@veetik.com>
2023-01-11 11:07:01 +0200

    add key to loadingCard

--- apps/frontend/src/components/_ui/Cards/LoadingCard.tsx
+++ apps/frontend/src/components/_ui/Cards/LoadingCard.tsx
@@ -6,11 +6,13 @@ import { Card } from "./Card";
 
 type Props = {
 	message: string;
+	myKey?: string;
 };
 
-export function LoadingCard({ message }: Props) {
+export function LoadingCard({ message, myKey }: Props) {
 	return (
 		<Card
+			key={myKey}
 			as={motion.div}
 			{...animateOpacityProps}
 			className="flex animate-pulse items-center justify-center rounded-xl px-3 py-5 font-light"
